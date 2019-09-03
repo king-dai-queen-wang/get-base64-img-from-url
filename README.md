6da558fcd55dee587b84653208f055c3a9732887
king-dai-queen-wang <1006889428@qq.com>
2019-09-03 16:26:28 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# get-img-from-url-observable-angular-vpgszw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/get-img-from-url-observable-angular-vpgszw)
---
+++ app/app.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  name = 'Angular 4';
+  base64Image: any;
+
+  ngOnInit() {
+    let imageUrl = 'https://i.imgur.com/hdcbPeO.jpg';
+
+    this.getBase64ImageFromURL(imageUrl).subscribe(base64data => {
+      console.log(base64data);
+      this.base64Image = 'data:image/jpg;base64,' + base64data;
+    });
+  }
+
+  getBase64ImageFromURL(url: string) {
+    return Observable.create((observer: Observer<string>) => {
+      let img = new Image();
+      img.crossOrigin = 'Anonymous';
+      img.src = url;  img.src = url;
+      if (!img.complete) {
+        img.onload = () => {
+          observer.next(this.getBase64Image(img));
+          observer.complete();
+        };
+        img.onerror = (err) => {
+          observer.error(err);
+        };
+      } else {
+        observer.next(this.getBase64Image(img));
+        observer.complete();
+      }
+    });
+  }
+
+  getBase64Image(img: HTMLImageElement) {
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+    var dataURL = canvas.toDataURL("image/png");
+    console.log(dataURL);
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+  }
+
+}
